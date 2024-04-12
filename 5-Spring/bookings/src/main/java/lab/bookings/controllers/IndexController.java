4b4cfe2ec20a6dabe7c57c0f3cab2d8589045d5d
dlisiewicz <dawidlisiewicz2000@gmail.com>
2024-04-12 10:46:59 +0200

    Add lab 5

---
+++ 5-Spring/bookings/src/main/java/lab/bookings/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package lab.bookings.controllers;
+
+import jakarta.validation.Valid;
+import lab.bookings.models.Apartment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/welcome")
+    public String index(@RequestParam(required = false) String name,
+                        Model model) {
+        if (name == null || name.equals(""))
+            name = "Anonymous";
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
